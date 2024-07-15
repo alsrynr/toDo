bbc9a9bda4081e5c4dc59862a1c3a5850cd2fcfe
unknown <alisha.rauniyarals@gmail.com>
2024-07-15 22:55:13 +0545

    added users model

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connect = async()=> {
     const connected = await mongoose.connect('mongodb://127.0.0.1:27017/toDoDb');
     if(connected){
-        console.log("connected to mongoose")
+        console.log("connected to mongodb")
     }
     else{
         console.log("connection failed")

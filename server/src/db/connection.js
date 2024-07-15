13e976912454e8ba08427b2b539586ed05d69beb
unknown <alisha.rauniyarals@gmail.com>
2024-07-15 22:29:36 +0545

    mongodb connection setup

---
+++ server/src/db/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const connect = async()=> {
+    const connected = await mongoose.connect('mongodb://127.0.0.1:27017/toDoDb');
+    if(connected){
+        console.log("connected to mongoose")
+    }
+    else{
+        console.log("connection failed")
+    }
+}
+ module.exports = connect

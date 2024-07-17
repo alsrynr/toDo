8f0c62f4930a7115b408685c99ca26e2ccb55ca7
unknown <alisha.rauniyarals@gmail.com>
2024-07-17 17:07:28 +0545

    added user model

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -2,9 +2,47 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose
 
 const userSchema = new Schema({
-  title: String, 
+title: {
+    type :String,
+    required: true,
+    unique : true,
+    trim: true 
+}, 
+ username: {
+   type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    trim: true
+  },
+  
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+   
 });
-
 const User = mongoose.model('User', userSchema);
-
 module.exports = User
